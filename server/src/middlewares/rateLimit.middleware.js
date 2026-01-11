ed59c40fe0441ef7763b1f6de585d1d83cbb6c02
Spandan Das <sd672483@gmail.com>
2026-01-11 13:45:44 +0530

    Update rateLimit.middleware.js

--- server/src/middlewares/rateLimit.middleware.js
+++ server/src/middlewares/rateLimit.middleware.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // Limit each IP to 100 requests per windowMs
+  windowMs: 60 * 1000, // 1 minutes
+  max: 1000, // Limit each IP to 100 requests per windowMs
   standardHeaders: true,
   legacyHeaders: false,
 });
